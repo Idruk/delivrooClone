984458471db4db3983b6d974f46d116fb7b45763
idruk <idruk>
2021-09-03 00:14:14 +0200

    page config

--- vue.config.ts
+++ vue.config.ts
@@ -3,5 +3,9 @@ module.exports = {
     build: {
       assetsPublicPath: '/',
       assetsSubDirectory: 'static'
-    }
+    },
+
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/delivrooClone/'
+      : '/'
   }
